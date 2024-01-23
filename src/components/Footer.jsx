84389c48749db15137d5444880d06bad2a73936a
Jarrett Lyon <64499632+jlyon12@users.noreply.github.com>
2024-01-22 18:50:02 -0700

    Fix: username

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
 		<footer className="flex gap-4 justify-center items-center p-4 bg-slate-800">
 			<a
 				className="  transform hover:scale-105 hover:rotate-12"
-				href="https://github.com/probableactions/memory-card"
+				href="https://github.com/jlyon12/memory-card"
 			>
 				<img className="w-8" src={GitHubIcon} alt="gitHub Icon" />
 			</a>
